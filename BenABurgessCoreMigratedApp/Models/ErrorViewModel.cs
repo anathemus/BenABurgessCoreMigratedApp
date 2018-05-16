a3e33f0713ca4f81d0afec4d0b72905830bacbbe
Benjamin Burgess <anathemus@gmail.com>
2018-05-16 15:06:39 -0400

    Add project files.

---
+++ BenABurgessCoreMigratedApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BenABurgessCoreMigratedApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
